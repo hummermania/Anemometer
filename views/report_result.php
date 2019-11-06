3360a9d072c4d50777fe9e3fdc3246e33dd49dd1
Alexander Markevich <rabota.pmr@gmail.com>
2019-11-06 10:28:28 +0200

    Change checksum format from bigint to CHAR(32) for newer version of pt-query-digest

--- views/report_result.php
+++ views/report_result.php
@@ -59,7 +59,7 @@ $( function ()  {
 					<?php if ($c == 'checksum') { ?>
 						<td><a href="<?php echo site_url()."?action=show_query&datasource={$datasource}&checksum=".$row[$c]; ?>"><?php echo $row[$c]; ?></a></td>
 					<?php }  else if ($c == 'DIGEST') { ?>
-							<td><a href="<?php echo site_url()."?action=show_query&datasource={$datasource}&checksum=".$row[$c]; ?>"><?php echo $row[$c]; ?></a></td>
+                        <td><a href="<?php echo site_url()."?action=show_query&datasource={$datasource}&checksum=".$row[$c]; ?>"><?php echo $row[$c]; ?></a></td>
 					<?php } else { ?>
 						<td><?php echo $row[$c]; ?></td>
 					<?php } ?>
